1dfa2155594535cc86709cf8b938acf8651561d7
Mumtaz503 <132309236+Mumtaz503@users.noreply.github.com>
2024-01-22 16:31:31 +0000

    2nd commit

--- back-end/hardhat.config.js
+++ back-end/hardhat.config.js
@@ -67,6 +67,12 @@ module.exports = {
       {
         version: "0.8.0"
       },
+      {
+        version: "0.7.0"
+      },
+      {
+        version: "0.6.6"
+      }
     ]
   }
 };
--- back-end/test/unit/TokenSwap/single-token-swap.test.js
+++ back-end/test/unit/TokenSwap/single-token-swap.test.js
@@ -9,7 +9,13 @@ const { assert } = require("chai");
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Single Token Swap unit tests", function () {
-      let singleTokenSwap, daiToken, wethToken, deployer, user, signer;
+      let singleTokenSwap,
+        daiToken,
+        wethToken,
+        deployer,
+        user,
+        signer,
+        routerv2;
 
       beforeEach(async () => {
         deployer = (await getNamedAccounts()).deployer;
@@ -18,13 +24,18 @@ const { assert } = require("chai");
         await deployments.fixture(["SingleTokenSwap"]);
         singleTokenSwap = await ethers.getContract("SingleTokenSwap", deployer);
         daiToken = await ethers.getContractAt(
-          "IERC20",
+          "ERC20Interface",
           "0x6B175474E89094C44Da98b954EedeAC495271d0F",
           signer
         );
         wethToken = await ethers.getContractAt(
-          "IERC20",
+          "ERC20Interface",
           "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+          signer
+        );
+        routerv2 = await ethers.getContractAt(
+          "UniswapV2Router02",
+          "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
           signer
         );
       });
@@ -39,15 +50,34 @@ const { assert } = require("chai");
       });
 
       describe("swapExactInputSingle", () => {
-        beforeEach(async function() {
-            const tokenAmount = ethers.parseEther("100");
-            const tx = await daiToken.transfer(deployer, tokenAmount);
-            await tx.wait(1);
+        beforeEach(async function () {
+          const amountOutMin = 100;
+          const path = [
+            "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+            "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+          ];
+          const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
+
+          const transactionResponse = await routerv2.swapExactETHForTokens(
+            amountOutMin,
+            path,
+            deployer,
+            deadline,
+            {
+              value: ethers.parseEther("1")
+            }
+          );
+
+          const transactionReciept = await transactionResponse.wait(1);
         });
 
-        it("Should update the balance", async function() {
-            const balanceOfDeployer = await daiToken.balanceOf(deployer);
-            console.log(balanceOfDeployer);
+        it("Should update the balance", async function () {
+          const amountToSend = 100;
+          const approval = await daiToken.approve(singleTokenSwap.target, amountToSend);
+          console.log(approval);
+          const tx = await singleTokenSwap.swapExactInputSingle(amountToSend);
+          await tx.wait(1);
+          //Figure out why I'm not getting my WETH
         });
       });
     });
